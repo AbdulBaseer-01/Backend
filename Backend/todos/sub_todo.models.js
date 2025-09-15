8696337b71a15fbcd8c297b83d238d83d2c4e118
AbdulBaseer-01 <mohammdabdulbaseer8@gmail.com>
2025-09-15 19:43:37 +0530

    data modeling-Ecommerce

--- Backend/todos/sub_todo.models.js
+++ Backend/todos/sub_todo.models.js
@@ -15,4 +15,4 @@ const subTodoSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
-export const SubTodo = mongoose.model("SubTodo", subTodoSchema)
+export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
